3d7e5d423f1140e0a6ce7d3eeed748c5ef885853
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-20 17:07:32 +0300

    One

---
+++ src/MAin.java
@@ -0,0 +1,13 @@
+public class MAin {
+        //Integer.toBinaryString
+        public static void main(String...args){
+
+                   for (char i = 'a'; i <= 'w'; i++){
+
+                       System.out.println( Integer.toBinaryString(i) );
+
+                   }
+
+        }
+
+}
